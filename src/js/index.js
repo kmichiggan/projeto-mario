9cffa38661c6da80fecc2878a46e87141e9b8cf7
kmichiggan <121730287+kmichiggan@users.noreply.github.com>
2023-02-23 14:54:48 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const botaoTrailer = document.querySelector(".botao-trailer") ;
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal() {
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+     alternarModal();
+    video.setAttribute("src", linkDoVideo);    
+}); 
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+   video.setAttribute("src", "");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
